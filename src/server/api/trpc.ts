9b78a2cf77d89a0e1b8bfdcddd386689f9acbb54
sdharrin <138178989+sdharrin@users.noreply.github.com>
2023-10-25 12:20:07 -0700

    added re-validation

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -22,6 +22,12 @@ import { prisma } from "~/server/db";
 
 type CreateContextOptions = {
   session: Session | null;
+  revalidateSSG:
+    | ((
+        urlPath: string,
+        opts?: { unstable_onlyGenerated?: boolean | undefined } | undefined
+      ) => Promise<void>)
+    | null;
 };
 
 /**
@@ -37,6 +43,7 @@ type CreateContextOptions = {
 export const createInnerTRPCContext = (opts: CreateContextOptions) => {
   return {
     session: opts.session,
+    revalidateSSG: opts.revalidateSSG,
     prisma,
   };
 };
@@ -55,6 +62,7 @@ export const createTRPCContext = async (opts: CreateNextContextOptions) => {
 
   return createInnerTRPCContext({
     session,
+    revalidateSSG: res.revalidate,
   });
 };
 
